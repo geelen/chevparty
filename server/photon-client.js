bf565055e838ced7b2e23b64d06ae638239e11f1
Glen Maddern <glenmaddern@gmail.com>
2015-12-03 21:41:05 +1100

    udp with a full buffer size seems sooo much easier

--- server/photon-client.js
+++ server/photon-client.js
@@ -2,6 +2,8 @@ require('dotenv').load()
 require('isomorphic-fetch')
 import url from 'url'
 import net from 'net'
+import dgram from 'dgram'
+let socket = dgram.createSocket('udp4')
 
 const state = {}
 export default state
@@ -10,10 +12,14 @@ let cloudUrl = `https://api.particle.io/v1/devices/${process.env.PARTICLE_DEVICE
 fetch(cloudUrl)
   .then(response => response.json())
   .then(data => {
-    if (!data.result) console.err(data)
+    console.log(data)
     let {port, hostname} = url.parse(data.result)
-    state.port = port
-    state.hostname = hostname
+    state.send = message => {
+      socket.send(message, 0, message.length, port, hostname, err => {
+        if (err) throw err
+        console.log(`UDP message sent to ${hostname}:${port}`)
+      })
+    }
     //state.client = net.connect(parseInt(port), hostname, () => console.log("Connected!"))
     //state.client.on('data', data => console.log(`${data}`))
   })
--- server/start.js
+++ server/start.js
@@ -1,15 +1,12 @@
 import state from './photon-client'
 import {Server} from 'ws'
-var dgram = require('dgram')
 
 let server = new  Server({port: 8901})
 server.on('connection', ws => {
   console.log(`Established WebSocket connection!`)
   ws.on('message', (message, flags) => {
-    console.log(message.length)
-    console.log(`Received from WebSocket: ${message.toString('hex')}`)
-    //if (state.client) {
-      //state.client.write("a" + message.slice(0, 50))
-    //}
+    //console.log(message.length)
+    //console.log(`Received from WebSocket: ${message.toString('hex')}`)
+    if (state.send) state.send(message)
   })
 })
